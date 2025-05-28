99136f7413bf58155dbb58465973dbd4cf5c814a
Sathya <neelasathya06@gmail.com>
2025-05-28 21:53:31 +0530

    store created and implemented with including display component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Counter from "./Counter.jsx";
+import Display from "./Display.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <div>
       <h1>Hello World!</h1>
         <Counter />
+      <Display />
     </div>
   )
 }
